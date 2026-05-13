eb80d6445caa0a9927c107821ae3f8541789b50a
Destro25 <alexandrualin1010@gmail.com>
2026-05-13 22:21:19 +0300

    Initial commit: Structura proiect GameRent

---
+++ src/main/java/com/awbd/gamerent/GamerentApplication.java
@@ -0,0 +1,13 @@
+package com.awbd.gamerent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamerentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamerentApplication.class, args);
+	}
+
+}
